d44356288d9fec0f88524c2cd0e1db94d608a42f
Dustin Thostenson <dustin@delta3consulting.com>
2025-09-09 10:53:41 -0500

    AnAuto test completed

--- library/src/test/java/com/langrsoft/slides/AnAuto.java
+++ library/src/test/java/com/langrsoft/slides/AnAuto.java
@@ -1,13 +1,21 @@
 package com.langrsoft.slides;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AnAuto {
+    private Auto auto;
+
+    @BeforeEach
+    void setUp() {
+        auto = new Auto();
+    }
+
     @Test
     void idlesEngineWhenStarted() {
-        var auto = new Auto();
         auto.DepressBrake();
 
         auto.PressStartButton();
@@ -15,15 +23,32 @@ class AnAuto {
         assertThat(auto.RPM()).isGreaterThan(950).isLessThan(1100);
     }
 
+    @Test
+    void doesNotStartWhenBrakeNotDepressed() {
+        assertThrows(BrakeNotEngagedException.class, () ->
+                auto.PressStartButton());
+    }
+
+
     static class Auto {
+
+        private boolean brakeDepressed;
+
         public void DepressBrake() {
+            brakeDepressed = true;
         }
 
         public void PressStartButton() {
+            if (!brakeDepressed) {
+                throw new BrakeNotEngagedException();
+            }
         }
 
         public int RPM() {
             return 1000;
         }
+    }
+
+    private static class BrakeNotEngagedException extends RuntimeException {
     }
 }
